f3a4788a0969349e703c0bca20dbeadd1c5d9bc8
Kashish Godha <connect2tech.in@gmail.com>
2020-07-27 13:32:47 +0530

    done

---
+++ src/loops/ForLoopExample.java
@@ -0,0 +1,17 @@
+package loops;
+
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		//Java Program to demonstrate the example of for loop  
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1; j<=3;j++)
+			{
+				System.out.println(i + " " + j);
+			}
+		}
+	}
+
+}
