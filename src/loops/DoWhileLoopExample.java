f89264da7610c328fea7715912dd602daa3661b3
unknown <connect2tech.in@gmail.com>
2020-08-26 18:19:10 +0530

    checkin

---
+++ src/loops/DoWhileLoopExample.java
@@ -0,0 +1,16 @@
+
+public class DoWhileLoopExample {
+
+	public static void main(String[] args) {
+		//Java Program to demonstrate the example of do-while loop  
+		int i=1;
+		do
+		{
+			System.out.println(i);
+			i++;
+		}
+		while(i<5);
+		
+	}
+
+}
---
+++ src/loops/WhileLoopExample.java
@@ -0,0 +1,14 @@
+
+public class WhileLoopExample {
+
+	public static void main(String[] args) {
+		//Java Program to demonstrate the example of while loop  
+		int i=5;
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
