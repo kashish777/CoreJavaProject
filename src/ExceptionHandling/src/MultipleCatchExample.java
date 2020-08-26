7af69df9b77f41f05049d30a53d70d7dfadd362d
unknown <connect2tech.in@gmail.com>
2020-08-26 19:09:38 +0530

    checkin

---
+++ src/ExceptionHandling/src/MultipleCatchExample.java
@@ -0,0 +1,27 @@
+
+public class MultipleCatchExample {
+
+		public static void main(String[] args) 
+		  {  
+		    try
+		    {    
+		      int a[]=new int[5];    
+		      System.out.println(a[17]);  
+		    }    
+		    catch(ArithmeticException e)  
+		    {  
+		      System.out.println("Arithmetic Exception --> "+e);  
+		    }    
+		    catch(ArrayIndexOutOfBoundsException e)  
+		    {  
+		      System.out.println("ArrayIndexOutOfBounds Exception --> "+e);  
+		    }    
+		    catch(Exception e)  
+		    {  
+		      System.out.println(e);  
+		    }   
+		    System.out.println("Exception is Handled.");
+		    
+	}
+
+}
